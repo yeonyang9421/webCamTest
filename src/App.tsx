e13145ca9dbaa6a4f371d351aa05eecacade884c
yeonyang9421 <yeonyang21@gmail.com>
2024-06-17 11:45:11 +0900

    first project commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+
+function App() {
+
+  function handleTakePhoto (dataUri: string) {
+    // Do stuff with the photo...
+    console.log('takePhoto',dataUri );
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+            <Camera
+      onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }
+    />
+      </header>
+    </div>
+  );
+}
+
+export default App;
